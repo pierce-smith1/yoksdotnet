9f0f690e12bfbb2099e6e21b76cf5cf612c88a9a
pierce-smith1 <pierce.smith@rezstream.com>
2024-10-31 22:59:05 -0600

    (#21) initial perlin noise generator

---
+++ logic/PerlinNoise.cs
@@ -0,0 +1,174 @@
+using System;
+
+namespace yoksdotnet.logic;
+
+public static class PerlinNoise
+{
+    private readonly ref struct NoiseVector
+    {
+        public required double X { get; init; }
+        public required double Y { get; init; }
+        public required double Z { get; init; }
+
+        public NoiseVector Subtract(NoiseVector other)
+        {
+            NoiseVector difference = new() 
+            { 
+                X = X - other.X,
+                Y = Y - other.Y,
+                Z = Z - other.Z,
+            };
+            return difference;
+        }
+
+        public double Dot(NoiseVector other)
+        {
+            var product = X * other.X + Y * other.Y + Z * other.Z;
+            return product;
+        }
+    }
+
+    private readonly ref struct CellVectors
+    {
+        public required NoiseVector A { get; init; }
+        public required NoiseVector B { get; init; }
+        public required NoiseVector C { get; init; }
+        public required NoiseVector D { get; init; }
+        public required NoiseVector E { get; init; }
+        public required NoiseVector F { get; init; }
+        public required NoiseVector G { get; init; }
+        public required NoiseVector H { get; init; }
+    }
+
+    private readonly ref struct CellDots
+    {
+        public required double A { get; init; }
+        public required double B { get; init; }
+        public required double C { get; init; }
+        public required double D { get; init; }
+        public required double E { get; init; }
+        public required double F { get; init; }
+        public required double G { get; init; }
+        public required double H { get; init; }
+    }
+
+    // Our RNG has to be REALLY fast here because it will be called
+    // for every sprite multiple times a frame.
+    // C# also does not provide a way to seed its standard Random class
+    // without creating a new one, which is unacceptable for this use case.
+    // So after an extremely minimal amount of research, I've decided to roll an xorshift.
+    // Adapted from https://en.wikipedia.org/wiki/Xorshift
+    private class FastRandom
+    {
+        private uint _state;
+
+        public void SetSeed(uint seed)
+        {
+            _state = seed;
+        }
+
+        public uint NextInt()
+        {
+            _state ^= _state << 13;
+            _state ^= _state >> 17;
+            _state ^= _state << 5;
+            return _state;
+        }
+
+        public double NextDouble()
+        {
+            var x = NextInt();
+            return (double)x / uint.MaxValue;
+        }
+    }
+
+    private static readonly FastRandom _rng = new();
+    private static readonly uint _baseSeed = (uint)DateTimeOffset.Now.Millisecond;
+
+    private static NoiseVector GetGradVector(double x, double y, double z)
+    {
+        double seed = _baseSeed;
+        seed = 31 * seed + x;
+        seed = 31 * seed + y;
+        seed = 31 * seed + z;
+
+        _rng.SetSeed((uint)seed);
+
+        double fX = _rng.NextDouble();
+        double fY = _rng.NextDouble();
+        double fZ = _rng.NextDouble();
+
+        double mag = Math.Sqrt(fX * fX + fY * fY + fZ * fZ);
+        fX /= mag;
+        fY /= mag;
+        fZ /= mag;
+
+        return new() { X = fX, Y = fY, Z = fZ };
+    }
+
+    private static NoiseVector GetGradVectorFromCorner(NoiseVector v)
+    {
+        var gradVector = GetGradVector(Math.Round(v.X), Math.Round(v.Y), Math.Round(v.Z));
+        return gradVector;
+    }
+
+    private static CellVectors GetCellCorners(NoiseVector v)
+    {
+        double x = Math.Floor(v.X);
+        double y = Math.Floor(v.Y);
+        double z = Math.Floor(v.Z);
+
+        return new()
+        {
+            A = new() { X = x,       Y = y,       Z = z       },
+            B = new() { X = x + 1.0, Y = y,       Z = z       },
+            C = new() { X = x,       Y = y + 1.0, Z = z       },
+            D = new() { X = x,       Y = y,       Z = z + 1.0 },
+            E = new() { X = x + 1.0, Y = y + 1.0, Z = z       },
+            F = new() { X = x + 1.0, Y = y,       Z = z + 1.0 },
+            G = new() { X = x,       Y = y + 1.0, Z = z + 1.0 },
+            H = new() { X = x + 1.0, Y = y + 1.0, Z = z + 1.0 },
+        };
+    }
+
+    private static CellVectors GetOffsetVectors(NoiseVector v)
+    {
+        var corners = GetCellCorners(v);
+
+        return new()
+        {
+            A = v.Subtract(corners.A),
+            B = v.Subtract(corners.B),
+            C = v.Subtract(corners.C),
+            D = v.Subtract(corners.D),
+            E = v.Subtract(corners.E),
+            F = v.Subtract(corners.F),
+            G = v.Subtract(corners.G),
+            H = v.Subtract(corners.H)
+        };
+    }
+
+    private static double CellInterpolate(ref readonly CellDots dots, NoiseVector v)
+    {
+        double wX = v.X - Math.Floor(v.X);
+        double wY = v.Y - Math.Floor(v.Y);
+        double wZ = v.Z - Math.Floor(v.Z);
+
+        double bottomI1 = Interpolate(dots.A, dots.B, wX);
+        double bottomI2 = Interpolate(dots.C, dots.E, wX);
+        double bottom = Interpolate(bottomI1, bottomI2, wY);
+
+        double topI1 = Interpolate(dots.C, dots.E, wX);
+        double topI2 = Interpolate(dots.G, dots.H, wX);
+        double top = Interpolate(topI1, topI2, wY);
+
+        double result = Interpolate(bottom, top, wZ);
+        return result;
+    }
+
+    private static double Interpolate(double a, double b, double w)
+    {
+        var result = (b - a) * (3.0 - w * 2.0) * w * w + a;
+        return result;
+    }
+}
